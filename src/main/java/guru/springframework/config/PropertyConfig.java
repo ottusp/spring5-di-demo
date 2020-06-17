503f5b90f0b4b53b1157435358cd1171102e2c26
euseikodak <ottofernandes2012@gmail.com>
2020-06-17 16:08:05 -0300

    Added a lot of property files, using both .properties and .yml files.

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -2,21 +2,14 @@ package guru.springframework.config;
 
 import guru.springframework.examplebeans.FakeDataSource;
 import guru.springframework.examplebeans.FakeJmsBroker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource({"classpath:example.properties", "classpath:jms.properties"})
 public class PropertyConfig {
 
-    @Autowired
-    Environment env;
-
-    @Value("${guru.user}")
+    @Value("${guru.username}")
     String user;
 
     @Value("${guru.password}")
