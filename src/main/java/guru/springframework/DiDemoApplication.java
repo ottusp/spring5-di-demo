fd8609158997d7adc514179737b84e128ff581d3
euseikodak <ottofernandes2012@gmail.com>
2020-06-16 21:34:12 -0300

    Added "injection" of properties by a file

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -1,12 +1,19 @@
 package guru.springframework;
 
+import guru.springframework.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiDemoApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+
+		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+
+		System.out.println(fakeDataSource.getUsername());
 	}
+
 }
